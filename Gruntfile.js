734cd672a95768f305ed3fd186f5fc033f280245
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-03-22 21:20:37 -0300

    Minify CSS with grunt-contrib-cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,26 @@ module.exports = function(grunt) {
         src: 'js/lib.js',
         dest: 'js/lib.min.js'
       }
+    },
+    cssmin: {
+      with_banner: {
+        options: {
+          banner: '/* Minified CSS File of <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+        },
+        files: {
+          'css/style.min.css': ['css/*.css']
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
